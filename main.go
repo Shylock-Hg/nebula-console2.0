0835ac2ab165730b55cf97a476e8e15b0756159f
Shylock Hg <33566796+Shylock-Hg@users.noreply.github.com>
2020-04-16 13:25:42 +0800

    Package cli to sperate file.

--- main.go
+++ main.go
@@ -7,21 +7,17 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
 	"strconv"
 	"time"
-	"path"
 
 	ngdb "github.com/shylock-hg/nebula-go2.0"
 	common "github.com/shylock-hg/nebula-go2.0/nebula"
 	graph "github.com/shylock-hg/nebula-go2.0/nebula/graph"
-	readline "github.com/shylock-hg/readline"
 )
 
 const NebulaLabel = "Nebula-Console"
@@ -230,82 +226,6 @@ func printResp(resp *graph.ExecutionResponse, duration time.Duration) {
 	fmt.Println()
 }
 
-const ttyColorPrefix = "\033["
-const ttyColorSuffix = "m"
-const ttyColorRed = "31"
-const ttyColorBold = "1"
-const ttyColorReset = "0"
-
-// Space name
-// Is error
-func prompt(space string, user string, isErr bool, isTTY bool) {
-	fmt.Println()
-	// (user@nebula) [(space)] >
-	if isTTY {
-		fmt.Printf("%s%s%s", ttyColorPrefix, ttyColorBold, ttyColorSuffix)
-	}
-	if isTTY && isErr {
-		fmt.Printf("%s%s%s", ttyColorPrefix, ttyColorRed, ttyColorSuffix)
-	}
-	fmt.Printf("(%s@%s) [(%s)]> ", user, NebulaLabel, space)
-	if isTTY {
-		fmt.Printf("%s%s%s", ttyColorPrefix, ttyColorReset, ttyColorSuffix)
-	}
-}
-
-type Cli interface {
-	Prompt(space string, isErr bool)
-	ReadLine() (string, error)
-	Interactive() bool
-}
-
-// interactive
-type iCli struct {
-	input *readline.Instance
-	user string
-	isTTY bool
-}
-
-func NewiCli(i *readline.Instance, user string, isTTY bool) iCli {
-	return iCli{i, user, isTTY}
-}
-
-func (l iCli) Prompt(space string, isErr bool) {
-	prompt(space, l.user, isErr, l.isTTY)
-}
-
-func (l iCli) ReadLine() (string, error) {
-	return l.input.Readline()
-}
-
-func (l iCli) Interactive() bool {
-	return true
-}
-
-// non-interactive
-type nCli struct {
-	input io.Reader
-	user  string
-	io *bufio.Reader
-}
-
-func NewnCli(i io.Reader, user string) nCli {
-	return nCli{i, user, bufio.NewReader(i)}
-}
-
-func (l nCli) Prompt(space string, isErr bool) {
-	// nothing
-}
-
-func (l nCli) ReadLine() (string, error) {
-	s, _, e := l.io.ReadLine()
-	return string(s), e
-}
-
-func (l nCli) Interactive() bool {
-	return false
-}
-
 // Loop the request util fatal or timeout
 // We treat one line as one query
 // Add line break yourself as `SHOW \<CR>HOSTS`
@@ -313,13 +233,10 @@ func loop(client *ngdb.GraphClient, c Cli) error {
 	c.Prompt("", false)
 	currentSpace := ""
 	for true {
-		line, err := c.ReadLine()
+		line, err, exit := c.ReadLine()
 		lineString := string(line)
-		if err != nil {
+		if  exit {
 			log.Printf("Get line failed: ", err.Error())
-			if err == io.EOF || err == readline.ErrInterrupt {
-				return nil
-			}
 			return err
 		}
 		if len(line) == 0 {
@@ -347,92 +264,6 @@ func loop(client *ngdb.GraphClient, c Cli) error {
 	}
 	return nil
 }
-
-var completer = readline.NewPrefixCompleter(
-	// show
-	readline.PcItem("SHOW",
-		readline.PcItem("HOSTS"),
-		readline.PcItem("SPACES"),
-		readline.PcItem("PARTS"),
-		readline.PcItem("TAGS"),
-		readline.PcItem("EDGES"),
-		readline.PcItem("USERS"),
-		readline.PcItem("ROLES"),
-		readline.PcItem("USER"),
-		readline.PcItem("CONFIGS"),
-	),
-
-	// describe
-	readline.PcItem("DESCRIBE",
-		readline.PcItem("TAG"),
-		readline.PcItem("EDGE"),
-		readline.PcItem("SPACE"),
-	),
-	readline.PcItem("DESC",
-		readline.PcItem("TAG"),
-		readline.PcItem("EDGE"),
-		readline.PcItem("SPACE"),
-	),
-	// get configs
-	readline.PcItem("GET",
-		readline.PcItem("CONFIGS"),
-	),
-	// create
-	readline.PcItem("CREATE",
-		readline.PcItem("SPACE"),
-		readline.PcItem("TAG"),
-		readline.PcItem("EDGE"),
-		readline.PcItem("USER"),
-	),
-	// drop
-	readline.PcItem("DROP",
-		readline.PcItem("SPACE"),
-		readline.PcItem("TAG"),
-		readline.PcItem("EDGE"),
-		readline.PcItem("USER"),
-	),
-	// alter
-	readline.PcItem("ALTER",
-		readline.PcItem("USER"),
-		readline.PcItem("TAG"),
-		readline.PcItem("EDGE"),
-	),
-
-	// insert
-	readline.PcItem("INSERT",
-		readline.PcItem("VERTEX"),
-		readline.PcItem("EDGE"),
-	),
-	// update
-	readline.PcItem("UPDATE",
-		readline.PcItem("CONFIGS"),
-		readline.PcItem("VERTEX"),
-		readline.PcItem("EDGE"),
-	),
-	// upsert
-	readline.PcItem("UPSERT",
-		readline.PcItem("VERTEX"),
-		readline.PcItem("EDGE"),
-	),
-	// delete
-	readline.PcItem("DELETE",
-		readline.PcItem("VERTEX"),
-		readline.PcItem("EDGE"),
-	),
-
-	// grant
-	readline.PcItem("GRANT",
-		readline.PcItem("ROLE"),
-	),
-	// revoke
-	readline.PcItem("REVOKE",
-		readline.PcItem("ROLE"),
-	),
-	// change password
-	readline.PcItem("CHANGE",
-		readline.PcItem("PASSWORD"),
-	),
-)
 
 func main() {
 	address := flag.String("address", "127.0.0.1", "The Nebula Graph IP address")
@@ -467,23 +298,8 @@ func main() {
 	// Loop the request
 	var exit error = nil
 	if interactive {
-		r, err := readline.NewEx(&readline.Config{
-				// TODO(shylock) prompt the space and error color
-				Prompt:          "nebula> ",
-				HistoryFile:     path.Join(historyHome, ".nebula_history"),
-				AutoComplete:    completer,
-				InterruptPrompt: "^C",
-				EOFPrompt:       "",
-				HistorySearchFold:   true,
-				FuncFilterInputRune: nil,
-			})
-		if err != nil {
-			log.Fatalf("Create readline failed, %s.", err.Error())
-		}
 
-		isTTY := readline.IsTerminal(int(os.Stdout.Fd()))
-
-		exit = loop(client, NewiCli(r, *username, isTTY))
+		exit = loop(client, NewiCli(historyHome, *username))
 	} else if *script != "" {
 		exit = loop(client, NewnCli(strings.NewReader(*script), *username))
 	} else if *file != "" {
